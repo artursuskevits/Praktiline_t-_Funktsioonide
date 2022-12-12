0a32ea7ad571e37ae6b9f164bc5edf174ad97763
opilane <opilane@HP-CZC5261P8F>
2022-12-12 13:34:14 +0200

    Add project files.

---
+++ Praktiline_töö_Funktsioonide.py
@@ -0,0 +1,61 @@
+from random import *
+
+from math import *
+
+#c = float(input("ümbermõõt?"))
+#D =round (c/pi,2) 
+#print(f"Puu läbimõõt = {D}") #Puu läbimõõdu arvutamine
+
+#N=float(input("side 1?"))
+#M=float(input("side 2?"))
+#D=round(sqrt(N**2+M**2)),2
+#print(f"ristküliku diagonaal = {D}") #Arvutage Pythoni käsureal
+
+#aeg = float(input("Mitu tundi kulus sõiduks? "))
+#teepikkus = float(input("Mitu kilomeetrit sõitsid? "))
+#kiirus = teepikkus / aeg
+#print("Sinu kiirus oli " + str(kiirus) + " km/h") #Leidke järgnevast näiteprogrammist semantiline viga:
+
+#a=int(input("number 1?"))
+#b =int(input("number 2?"))
+#c =int(input("number 3?"))
+#d =int(input("number 4?"))
+#e =int(input("number 5?"))
+
+#arv = a+b+c+d +e
+#arv = arv/5 
+#print(f"keskmine: = {arv}") #Koostada programm, mis arvutab aritmeetilise keskmise suvalisest etteantud 5 täis arvus
+
+#print("   @..@   ")
+#print("  (----)") 
+#print(" ( \__/ )") 
+#print(' ^^ "" ^^ ') #Joonista samasugune konn
+
+#a=randint(0,100)
+#b =randint(0,100)
+#c =randint(0,100)
+#print(f"a={a}\nb={b}\nc={c}")
+#P = a+b+c
+#print(f"kolmnurga ümbermõõt = {P}") #(P=a+b+c)
+
+#p= randint(1,6)
+#summa=(12.9*1.1)/p 
+#print(f"{p}-st, Igaüks maksab {summa}") #Pitsa
+
+#print("küttusekulu arvutamine")
+#l=float(input("kütuse liitride kogus: "))
+#km=float(input("Länitud kilomeetrid:" ))
+#kuulu = round((l/km)*100),2
+#print(f"kuulu :{kuulu}") #Kütusekulu arvutamine
+
+
+#M=int(input("minutid: "))
+#M=M/60
+#tee=M*29,9
+#print(f"Jõulab {tee} km") #Rulluisutajad
+
+#print("Ajatesindus")
+#M=int(input("Siseta aja minutites")) #1h=60min
+#H=M//60 #h
+#M=M%60 #m
+#print(f"{H}:{M}") #Ajateisendus
